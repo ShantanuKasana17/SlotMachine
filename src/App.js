e8a17ecd35ae8e9922867416d1fce6aec6607e9d
Shantanu <shantanukasana017@gmail.com>
2023-10-22 12:17:56 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SlotM from './SlotMachine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  return <>
+    <h1 className="heading_style"> 
+    😎Welcome to {" "} <span style = {{fontWeight: "bold"}}> Slot Machine game </span>😎
+    </h1>
+    <div className="slotmachine">
+    <SlotM x="😂" y="😂" z="😂"/>
+    <hr />
+    <SlotM x="😊" y="😎"  z="😉"/>
+    <hr />
+    <SlotM x="🍎"y="🥭" z="🍌"/>
+    <hr />
+    <SlotM x="🙌"y="🙌" z="🙌"/>
+
     </div>
-  );
+    
+  </>
 }
 
 export default App;
